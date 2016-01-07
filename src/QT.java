0a710a93b5dcad34c662a7604e97e6e89fe83504
CalebBain <cbain@student.neumont.edu>
2016-01-06 23:21:15 -0700

    working workning font functions for button are Font-Family, Font-Size, and Font-weight. Font-Style is still not working just.

--- src/QT.java
+++ src/QT.java
@@ -3,6 +3,8 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import com.trolltech.qt.gui.*;
 
+import java.util.List;
+
 public class QT extends QApplication{
     private Node Window;
 
@@ -31,7 +33,8 @@ public class QT extends QApplication{
         Node h = nodeMap.getNamedItem("height");
         Node w = nodeMap.getNamedItem("width");
         window.setWindowTitle(tr((!title.isEmpty())? title : "JAML Applicaiton"));
-        window.resize(((w != null) ? Integer.parseInt(w.getNodeValue()) : 400), ((h != null) ? Integer.parseInt(h.getNodeValue()) : 200));
+        window.resize(((w != null) ? Integer.parseInt(w.getNodeValue()) : 400),
+                ((h != null) ? Integer.parseInt(h.getNodeValue()) : 200));
         return window;
     }
 
@@ -42,5 +45,44 @@ public class QT extends QApplication{
         String w = nodeMap.getNamedItem("width").getNodeValue();
         button.setText(tr(Button.getTextContent()));
         button.resize(((!w.isEmpty())? Integer.parseInt(w) : 20), ((!h.isEmpty())? Integer.parseInt(h) : 10));
+        button.setFont(Font(nodeMap.getNamedItem("font-family").getNodeValue(),
+                nodeMap.getNamedItem("font-size").getNodeValue(), nodeMap.getNamedItem("font-style").getNodeValue(),
+                nodeMap.getNamedItem("font-weight").getNodeValue()));
+    }
+
+    public QFont Font(String family, String size, String style, String weight) {
+        QFont font = new QFont();
+        QFontDatabase database = new QFontDatabase();
+        List<String> families = database.families();
+        List<Integer> sizes = database.pointSizes(family);
+        List<String> styles = database.styles(family);
+        if(families.contains(family)) font.setFamily(family);
+        try {
+            int Size = Integer.parseInt(size);
+            if(sizes.contains(Size)) font.setPixelSize(Size);
+        }catch (NumberFormatException nfe){}
+        QFont.Style Style = styles.get(0);
+
+
+        font.setStyle(Style);
+        font.setWeight(bold(weight));
+        return font;
+    }
+
+    public int bold(String keyword){
+        int weight = 50;
+        try {
+            weight = Integer.parseInt(keyword);
+        }catch (NumberFormatException nfe){
+            switch(keyword){
+                case "lighter":     weight = 15; break;
+                case "light":       weight = 25; break;
+                case "normal":      weight = 50; break;
+                case "bold":        weight = 75; break;
+                case "bolder":      weight = 85; break;
+                default:            weight = 50;
+            }
+        }
+        return weight;
     }
 }
