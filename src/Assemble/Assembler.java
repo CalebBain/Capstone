16ad8ec9f569197f3e31528fd81214b69404c24d
CalebBain <cbain@student.neumont.edu>
2016-01-09 19:28:00 -0700

    Completed the style tokenizer

--- src/Assemble/Assembler.java
+++ src/Assemble/Assembler.java
@@ -1,9 +1,12 @@
 package Assemble;
 
+import StyleComponents.Style;
 import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Assembler {
     public static void  main(String[] args)
@@ -31,7 +34,7 @@ public class Assembler {
         if(node instanceof Element && node.getNodeName().equals("jaml")){
             Element docElement = (Element)node;
             Node style = docElement.getElementsByTagName("style").item(0);
-            if(style != null) new StyleParser();
+            if(style != null) new StyleParser(style);
             Node window = docElement.getElementsByTagName("window").item(0);
             if(window != null) ParseFlavor(window);
         }else throw new SyntaxException("the file must begin with a jaml tag");
@@ -48,13 +51,10 @@ public class Assembler {
     private void printNote(NodeList nodeList) {
         for (int count = 0; count < nodeList.getLength(); count++) {
             Node tempNode = nodeList.item(count);
-            // make sure it's element node.
             if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
-                // get node name and value
                 System.out.println("\nNode Name = " + tempNode.getNodeName() + " [OPEN]");
                 System.out.println("Node Value = " + tempNode.getTextContent());
                 if (tempNode.hasAttributes()) {
-                    // get attributes names and values
                     NamedNodeMap nodeMap = tempNode.getAttributes();
                     for (int i = 0; i < nodeMap.getLength(); i++) {
                         Node node = nodeMap.item(i);
@@ -63,7 +63,7 @@ public class Assembler {
                         System.out.println("attr value : " + node.getNodeValue());
                     }
                 }
-                if (tempNode.hasChildNodes()) printNote(tempNode.getChildNodes()); // loop again if has child nodes
+                if (tempNode.hasChildNodes()) printNote(tempNode.getChildNodes());
                 System.out.println("Node Name = " + tempNode.getNodeName() + " [CLOSE]");
             }
         }
--- src/Assemble/StyleParser.java
+++ src/Assemble/StyleParser.java
@@ -1,8 +1,83 @@
 package Assemble;
 
+import StyleComponents.Style;
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 /**
  * Created by Caleb Bain on 1/9/2016.
  */
 public class StyleParser {
+
+    public StyleParser(Node style) {
+        Map<String, Character> tokens = GetContent(style.getTextContent());
+    }
+
+    private Map<String, Character> GetContent(String s){
+        char currentState = 'a';
+        char[][] states = {{'a','b','~','~','e','~'}, {'a','b','c','~','e','~'},
+                           {'c','c','~','d','~','~'}, {'a','b','~','~','~','f'},
+                           {'a','b','~','~','~','f'}, {'a','b','~','~','~','~'}};
+        Map<String, Character> tokens = new HashMap<>();
+        char[] list = s.toCharArray();
+        int count = 0;
+        while(count++ < s.length()){
+            String token = "";
+            for(;count < s.length(); count++){
+                char c = list[count];
+                int j = 0, i = currentState - 97;
+                String t = c + "";
+                if(c == ' ') j = 0;
+                else if(t.matches("[A-z0-9\\-]")) j = 1;
+                else if(c == ':') j = 2;
+                else if(c == ';') j = 3;
+                else if(c == '{') j = 4;
+                else if(c == '}') j = 5;
+                char state = states[i][j];
+                if(state == 'a' || state == 'e') {
+                    if(!token.isEmpty()) tokens.put(token, state);
+                    currentState = state;
+                    break;
+                }
+                if(c != ';' && c!=' ') token += c;
+                currentState = state;
+            }
+
+        }
+        return tokens;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void ParseStyle(Node style){
+        List<Style> styles = new ArrayList<>();
+        //NamedNodeMap nodeMap = style.getAttributes();
+        //String link = nodeMap.getNamedItem("link").getNodeValue();
+        String s = style.getTextContent();
+
+    }
+
 
 }
