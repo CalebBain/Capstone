9cbe3ae3cc34c7bc5e33503c3d453cde5d2301c8
CalebBain <cbain@student.neumont.edu>
2016-02-12 15:11:26 -0700

    Fixed the compilation issue

--- src/Compiler/Assemble/Compiler.java
+++ src/Compiler/Assemble/Compiler.java
@@ -1,12 +1,9 @@
 package Compiler.Assemble;
 
 import Compiler.Parsers.ComponentParser;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 import org.w3c.dom.Node;
-
-import javax.tools.*;
-import java.io.*;
-import java.lang.reflect.Constructor;
-import java.net.URI;
 import java.util.*;
 
 public final class Compiler {
@@ -16,60 +13,16 @@ public final class Compiler {
 
     public void assemble(String file, Map<String, String> methodCalls, Node node){
         StringBuilder sb = new StringBuilder();
-        sb.append("package GeneratedCode;\n");
         sb.append("import com.trolltech.qt.core.*;\nimport com.trolltech.qt.gui.*;\n");
-        sb.append("public class qt extends QApplication{\npublic qt(boolean run) { super(new String[0]);\nif(run) run();\n}\n");
+        sb.append("public class qt extends QApplication{\npublic qt() { super(new String[0]); run(); }\n");
         sb.append("public void run() {\n");
         ComponentParser parser = new ComponentParser(file, methodCalls, sb, node);
         String styles = parser.StyleSheet();
         if (!styles.isEmpty()) sb.append(String.format("this.setStyleSheet(\"%s\");\n", styles));
         sb.append("window.show();\n");
         sb.append("this.exec();\n}\n}");
-        try(PrintWriter writer = new PrintWriter("src/GeneratedCode/qt.java", "UTF-8")) {
-            File f = new File("src/GeneratedCode/qt.java");
-            if(f.exists()) f.delete();
-            String Code = sb.toString();
-            System.out.println("Rewriting");
-            writer.println(Code);
-            System.out.println(Code);
-            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-            JavaFileObject qtCode = new InMemoryJavaFileObject("GeneratedCode.qt", Code);
-            Iterable<? extends JavaFileObject> files = Arrays.asList(qtCode);
-            final Iterable<String> options = Arrays.asList("-d", "out/production/JAML/GeneratedCode/");
-            JavaCompiler.CompilationTask task = compiler.getTask(null, null, null, options, null, files);
-            if (task.call()) {
-                Class<?> c = Class.forName("GeneratedCode.qt");
-                Constructor ctor = c.getDeclaredConstructor(Boolean.TYPE);
-                ctor.newInstance(true);
-            }
-            /*if (task.call()) {
-                System.out.println("Running");
-                Class<?> c = Class.forName("GeneratedCode.qt");
-                Constructor ctor = c.getDeclaredConstructor(Boolean.TYPE);
-                ctor.newInstance(true);
-            }
-            /*if(task.call()){
-
-                Class<?> c = Class.forName("GeneratedCode.qt");
-            }else{
-                callOnce = true;
-            }
-            Class<?> c = Class.forName("GeneratedCode.qt");
-            c.newInstance();*/
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-class InMemoryJavaFileObject extends SimpleJavaFileObject {
-    private String contents = null;
-    InMemoryJavaFileObject(String className, String contents) throws Exception {
-        super(URI.create("string:///" + className.replace('.', '/') + Kind.SOURCE.extension), Kind.SOURCE);
-        this.contents = contents;
-    }
-
-    public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
-        return contents;
+        String Code = sb.toString();
+        System.out.println(Code);
+        new GroovyShell(new Binding()).evaluate(Code);
     }
 }
